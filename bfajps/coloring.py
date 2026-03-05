ab22da974ae65a73620f07c354dc6727d80789e8
JaredPeSa <pe485451@uaeh.edu.mx>
2026-03-05 23:27:52 +0000

    biblioteca ordenada

---
+++ bfajps/coloring.py
@@ -0,0 +1,17 @@
+import itertools
+
+# programa de los colores
+
+def is_proper_coloring(graph, coloring):
+    for edge in graph.edges():
+        if coloring[edge[0]] == coloring[edge[1]]:
+            return False
+    return True
+
+def is_3_coloring(graph):
+    n = graph.order()
+    colorings = itertools.product([0,1,2], repeat = n)
+    for coloring in colorings:
+        if is_proper_coloring(graph, coloring):
+            return coloring
+    return False
---
+++ bfajps/hamiltonian.py
@@ -0,0 +1,27 @@
+import networkx as nx
+import itertools
+
+# programa para ver si es h
+
+def is_hamiltonian_cycle(graph, cycle):
+    """Checks if cycle is a hamiltonian cycle in graph.
+    Graph is a Networkx graph, and cycle is a list of vertices"""
+    n = len(set(cycle))
+    if n != graph.order():
+        return False
+    for i in range(n-1):
+        if not graph.has_edge(cycle[i], cycle[i+1]):
+            return False
+    if not graph.has_edge(cycle[n-1], cycle[0]):
+        return False
+    return True
+
+def is_hamiltonian(graph):
+    vertices = list(graph.nodes())
+    if len(vertices) < 3:
+        return False
+    perms = itertools.permutations(vertices)
+    for perm in perms:
+        if is_hamiltonian_cycle(graph, perm):
+            return perm
+    return False
